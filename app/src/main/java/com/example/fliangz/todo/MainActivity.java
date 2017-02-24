e7ecfced7a0642cbee51fd5a47b0d4ca0fc4f296
Liang <fliangz@ufl.edu>
2017-02-24 14:40:25 -0500

    WIP - integrating myscript

--- app/src/main/java/com/example/fliangz/todo/MainActivity.java
+++ app/src/main/java/com/example/fliangz/todo/MainActivity.java
@@ -1,11 +1,24 @@
 package com.example.fliangz.todo;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.app.AlertDialog.Builder;
+import android.app.AlertDialog;
+
+
+import com.myscript.atk.scw.SingleCharWidget;
+import com.myscript.atk.scw.SingleCharWidgetApi;
+import com.myscript.atk.core.ui.IStroker;
+import com.myscript.atk.scw.SingleCharWidgetApi;
+import com.myscript.atk.text.CandidateInfo;
+import com.example.fliangz.todo.MyCertificate;
 
 import org.apache.commons.io.FileUtils;
 
@@ -15,7 +28,14 @@ import java.util.ArrayList;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements
+        SingleCharWidgetApi.OnConfiguredListener,
+        SingleCharWidgetApi.OnTextChangedListener {
+
+    private static final String TAG = "SingleCharDemo";
+
+    private SingleCharWidgetApi widget;
 
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
@@ -54,6 +74,72 @@ public class MainActivity extends AppCompatActivity {
         lvItems.setAdapter(itemsAdapter);
         setupListViewListener();
 
+        widget = (SingleCharWidget) findViewById(R.id.singleChar_widget);
+        if (!widget.registerCertificate(com.example.fliangz.todo.MyCertificate.getBytes()))
+        {
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+            dlgAlert.setMessage("Please use a valid certificate.");
+            dlgAlert.setTitle("Invalid certificate");
+            dlgAlert.setCancelable(false);
+            dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    //dismiss the dialog
+                }
+            });
+            dlgAlert.create().show();
+            return;
+        }
+
+        widget.setOnConfiguredListener(this);
+        widget.setOnTextChangedListener(this);
+
+        // references assets directly from the APK to avoid extraction in application
+        // file system
+        widget.addSearchDir("zip://" + getPackageCodePath() + "!/assets/conf");
+
+        // The configuration is an asynchronous operation. Callbacks are provided to
+        // monitor the beginning and end of the configuration process and update the UI
+        // of the input method accordingly.
+        //
+        // "en_US" references the en_US bundle name in conf/en_US.conf file in your assets.
+        // "si_text" references the configuration name in en_US.conf
+        widget.configure("en_US", "si_text");
+
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        widget.setOnTextChangedListener(null);
+        widget.setOnConfiguredListener(null);
+
+        super.onDestroy();
+    }
+
+    @Override
+    public void onConfigured(SingleCharWidgetApi widget, boolean success)
+    {
+        if(!success)
+        {
+            Toast.makeText(getApplicationContext(), widget.getErrorString(), Toast.LENGTH_LONG).show();
+            Log.e(TAG, "Unable to configure the Single Char Widget: " + widget.getErrorString());
+            return;
+        }
+        Toast.makeText(getApplicationContext(), "Single Char Widget Configured", Toast.LENGTH_SHORT).show();
+        if(BuildConfig.DEBUG)
+            Log.d(TAG, "Single Char Widget configured!");
+    }
+
+    @Override
+    public void onTextChanged(SingleCharWidgetApi widget, String s, boolean intermediate)
+    {
+        Toast.makeText(getApplicationContext(), "Recognition update", Toast.LENGTH_SHORT).show();
+        if(BuildConfig.DEBUG)
+        {
+            Log.d(TAG, "Single Char Widget recognition: " + widget.getText());
+        }
     }
 
     private void setupListViewListener() {
@@ -70,11 +156,6 @@ public class MainActivity extends AppCompatActivity {
 
                 });
 
-        lvItems.setOnTouchListener(
-                new View.OnTouchListener() {
-
-                }
-        );
     }
 
     public void onAddItem(View v) {
